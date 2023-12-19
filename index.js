51cddff4acb8f665dc3380dd158462197dc4ce44
Benn <bennkiche7@gmail.com>
2023-12-19 14:44:28 -0800

    second commit

--- index.js
+++ index.js
@@ -1,5 +1,17 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(thefeet){
+ if (thefeet <=400) {
+  return "give the customers a free sample"
+ }
+ else if (thefeet >= 2000) {
+  return "I will gladly take your thirty bucks"
+ }
+ else if (thefeet >= 2500) {
+  return "No can do"
+ }
+ else if (thefeet = 199) {
+  return "This one is on me!"
+ }
+console.log (scuberGreetingForFeet (199))
 }
 
 function ternaryCheckCity(){
