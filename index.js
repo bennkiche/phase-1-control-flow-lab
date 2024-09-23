84dd254fb82e8fbc6ca75e0d8f9a93119b833620
Benn <bennkiche7@gmail.com>
2024-09-23 23:39:47 +0300

    completetask

--- index.js
+++ index.js
@@ -1,23 +1,27 @@
-function scuberGreetingForFeet(thefeet){
- if (thefeet <=400) {
-  return "give the customers a free sample"
- }
- else if (thefeet >= 2000) {
-  return "I will gladly take your thirty bucks"
- }
- else if (thefeet >= 2500) {
-  return "No can do"
- }
- else if (thefeet = 199) {
-  return "This one is on me!"
- }
+function scuberGreetingForFeet(distance) {
+  if (distance <= 400) {
+    return 'This one is on me!';
+  } else if (distance > 400 && distance <= 2000) {
+    return 'That will be $25.';
+  } else if (distance > 2000 && distance <= 2500) {
+    return 'I will gladly take your thirty bucks.';
+  } else {
+    return "No can do.";
+  }
+}
 console.log (scuberGreetingForFeet (199))
-}
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
+function ternaryCheckCity(city) {
+    return city === 'NYC' ? 'Ok, sounds good.' : 'No go.';
+  }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
-}
+  function switchOnCharmFromTip(tip) {
+    switch (tip) {
+      case 'generous':
+        return 'Thank you so much.';
+      case 'not as generous':
+        return 'Thank you.';
+      default:
+        return 'Bye.';
+    }
+  }
